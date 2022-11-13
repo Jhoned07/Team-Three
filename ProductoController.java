099bf07c612ced71286a20320ca1b90d9b95054b
John Naranjo <ingeprimazo@gmail.com>
2022-11-13 17:26:17 -0500

    mejora a producto controller

--- ProductoController.java
+++ ProductoController.java
@@ -3,13 +3,16 @@ package com.example.tienda.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tienda.model.Categoria;
 import com.example.tienda.model.Producto;
 import com.example.tienda.services.ProductoServices;
 
@@ -34,4 +37,14 @@ public class ProductoController {
     public ResponseEntity<?> crearProducto(@RequestBody Producto producto){
         return ResponseEntity.status(HttpStatus.CREATED).body(servicio.crearProducto(producto));
     }
+    
+    @DeleteMapping("/{id}")
+	public ResponseEntity<?> eliminarProducto(@PathVariable Long id){
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(servicio.eliminarProducto(id)); 
+	}
+    
+    @PutMapping("/{id}")
+	public ResponseEntity<?> actualizarProducto(@PathVariable Long id, @RequestBody Producto producto){
+		return ResponseEntity.status(HttpStatus.OK).body(servicio.actualizarProducto(producto));
+	}
 }
